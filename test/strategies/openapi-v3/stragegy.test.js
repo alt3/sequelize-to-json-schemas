2477a96388e6b31d4140ba18955f47448f72fff9
bravo-kernel <bravo-kernel@users.noreply.github.com>
2019-08-17 18:04:22 +0200

    Re-order tests for a more logical flow

--- test/strategies/openapi-v3/stragegy.test.js
+++ test/strategies/openapi-v3/stragegy.test.js
@@ -12,75 +12,84 @@ const userModel = sequelize.import('../../models/user.js'); // without `.build()
 
 describe('OpenAPI v3 strategy (#integration)', function() {
   describe('Default options', function() {
+    // ------------------------------------------------------------------------
+    // generate schema
+    // ------------------------------------------------------------------------
     const schemaManager = new SchemaManager();
     const strategy = new OpenApi3Strategy();
+    const schema = schemaManager.generate(userModel.build(), strategy);
 
     // ------------------------------------------------------------------------
-    // generate schema and insert into the wrapper
+    // confirm sequelize model properties render as expected
     // ------------------------------------------------------------------------
-    const modelSchema = schemaManager.generate(userModel.build(), strategy);
-    schemaWrapper.components.schemas.users = modelSchema;
+    describe('Sequelize model properties:', function() {
+      it("has property 'title' with value 'users'", function() {
+        expect(schema).toHaveProperty('title');
+        expect(schema.title).toEqual('User');
+      });
 
-    // convenience variable pointing to the model schema
-    const modelNode = schemaWrapper.components.schemas.users.properties;
+      it("has property 'type' with value 'object'", function() {
+        expect(schema).toHaveProperty('type');
+        expect(schema.type).toEqual('object');
+      });
+    });
 
     // ------------------------------------------------------------------------
-    // make sure the wrapper is properly generated
+    // confirm sequelize attributes render as expected
     // ------------------------------------------------------------------------
-    describe('Tree structure', function() {
-      it('has leaf ./openapi with string version 3.n.n', function() {
+    describe('Sequelize attributes:', function() {
+      describe('_STRING_ALLOWNULL_', function() {
+        it("has property 'type' of type 'string'", function() {
+          expect(schema.properties).toHaveProperty('_STRING_ALLOWNULL_');
+          expect(schema.properties._STRING_ALLOWNULL_).toHaveProperty('type');
+          expect(schema.properties._STRING_ALLOWNULL_.type).toEqual('string');
+        });
+
+        it("has property 'nullable' of type 'boolean'", function() {
+          expect(schema.properties._STRING_ALLOWNULL_).toHaveProperty('nullable');
+          expect(typeof schema.properties._STRING_ALLOWNULL_.nullable).toEqual('boolean');
+        });
+      });
+    });
+
+    // ------------------------------------------------------------------------
+    // confirm user-definable attribute properties render as expected
+    // ------------------------------------------------------------------------
+    describe('User definable attribute properties:', function() {
+      describe('_USER_DEFINED_PROPERTIES_', function() {
+        it("has property 'description' of type 'string'", function() {
+          expect(schema.properties).toHaveProperty('_USER_DEFINED_PROPERTIES_');
+          expect(schema.properties._USER_DEFINED_PROPERTIES_).toHaveProperty('description');
+          expect(typeof schema.properties._USER_DEFINED_PROPERTIES_.description).toBe('string');
+        });
+
+        it("has property 'example' of type 'array'", function() {
+          expect(schema.properties._USER_DEFINED_PROPERTIES_).toHaveProperty('example');
+          expect(Array.isArray(schema.properties._USER_DEFINED_PROPERTIES_.example)).toBe(true);
+        });
+      });
+    });
+
+    // ------------------------------------------------------------------------
+    // confirm the document is valid OpenAPI 3.0
+    // ------------------------------------------------------------------------
+    describe('Document:', function() {
+      schemaWrapper.components.schemas.users = schema;
+
+      it("has leaf /openapi with string containing version '3.n.n'", function() {
         expect(schemaWrapper).toHaveProperty('openapi');
         expect(schemaWrapper.openapi).toMatch(/^3\.\d\.\d/); // 3.n.n
       });
 
-      it('has non-empty container ./components/schemas/users', function() {
+      it('has non-empty container /components/schemas/users', function() {
         expect(schemaWrapper.components.schemas).toHaveProperty('users');
         expect(Object.keys(schemaWrapper.components.schemas.users).length).toBeGreaterThan(0);
       });
-    });
 
-    // ------------------------------------------------------------------------
-    // make sure sequelize attributes are rendered as expected
-    // ------------------------------------------------------------------------
-    describe('Sequelize attributes', function() {
-      describe('_STRING_ALLOWNULL_', function() {
-        it("has property 'type' with value 'string'", function() {
-          expect(modelNode).toHaveProperty('_STRING_ALLOWNULL_');
-          expect(modelNode._STRING_ALLOWNULL_).toHaveProperty('type');
-          expect(modelNode._STRING_ALLOWNULL_.type).toEqual('string');
-        });
-
-        it("has property 'nullable' with value 'true'", function() {
-          expect(modelNode._STRING_ALLOWNULL_).toHaveProperty('nullable');
-          expect(modelNode._STRING_ALLOWNULL_.nullable).toBe(true);
-        });
-      });
-    });
-
-    // ------------------------------------------------------------------------
-    // make sure user-definable attribute properties are rendered as expected
-    // ------------------------------------------------------------------------
-    describe('User definable properties', function() {
-      describe('_USER_DEFINED_PROPERTIES_', function() {
-        it("has property 'description' of type 'string'", function() {
-          expect(modelNode).toHaveProperty('_USER_DEFINED_PROPERTIES_');
-          expect(modelNode._USER_DEFINED_PROPERTIES_).toHaveProperty('description');
-          expect(typeof modelNode._USER_DEFINED_PROPERTIES_.description).toBe('string');
-        });
-
-        it("has property 'example' of type 'array'", function() {
-          expect(modelNode._USER_DEFINED_PROPERTIES_).toHaveProperty('example');
-          expect(Array.isArray(modelNode._USER_DEFINED_PROPERTIES_.example)).toBe(true);
-        });
-      });
-    });
-
-    // ------------------------------------------------------------------------
-    // make sure the schema is valid OpenAPI 3.0 by running Swagger Parser.
-    // ------------------------------------------------------------------------
-    describe('Schema', function() {
+      // validate document using Swagger Parser
       it('successfully validates as JSON API 3.0', async () => {
         expect.assertions(1);
+
         // https://github.com/APIDevTools/swagger-parser/issues/77
         // @todo: enable once fixed, now blocks husky pre-commit hooks
         const result = await SwaggerParser.validate(_.cloneDeep(schemaWrapper));
@@ -93,7 +102,7 @@ describe('OpenAPI v3 strategy (#integration)', function() {
     //   console.log('Does nothing');
     // });
 
-    // @todo add this to the test suite, it should throw an exception
+    // @todo add this to the StrategyInterface test suite, it should throw an exception
     // Model.rawAttributes._FAKE_TYPE_ = {
     //   type: "FAKETYPE"
     // };
