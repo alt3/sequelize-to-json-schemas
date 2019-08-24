69a64511ee95e269b2fc7af767015618493d1cf6
bravo-kernel <bravo-kernel@users.noreply.github.com>
2019-08-24 22:22:11 +0200

    Move non-strategy-specific tests to the StrategyInterface test case

--- test/strategies/json-schema-v7-strategy.test.js
+++ test/strategies/json-schema-v7-strategy.test.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-unused-vars */
 
+/**
+ * Please note that we are ONLY testing strategy-specific behavior here. All
+ * non-strategy-specific tests are handled by the StrategyInterface test case.
+ */
 const Ajv = require('ajv');
 const models = require('../models');
-const supportedDataType = require('../utils/supported-datatype');
-
 const { SchemaManager, JsonSchema7Strategy } = require('../../lib');
 
 describe('JsonSchema7Strategy', function() {
@@ -18,7 +20,7 @@ describe('JsonSchema7Strategy', function() {
     // ------------------------------------------------------------------------
     // make sure sequelize model properties render as expected
     // ------------------------------------------------------------------------
-    describe('Ensure schema.model:', function() {
+    describe('Ensure model properties are rendered as expected and thus schema.model:', function() {
       const schemaUri = 'https://json-schema.org/draft-07/schema#';
       it(`has property '$schema' with value '${schemaUri}'`, function() {
         expect(schema.$schema).toEqual('https://json-schema.org/draft-07/schema#');
@@ -27,124 +29,42 @@ describe('JsonSchema7Strategy', function() {
       it("has property '$id' with value '/user.json'", function() {
         expect(schema.$id).toEqual('/user.json');
       });
-
-      it("has property 'title' with value 'users'", function() {
-        expect(schema.title).toEqual('User');
-      });
-
-      it("has property 'type' with value 'object'", function() {
-        expect(schema.type).toEqual('object');
-      });
     });
 
     // ------------------------------------------------------------------------
     // make sure sequelize DataTypes render as expected
     // ------------------------------------------------------------------------
     describe('Ensure Sequelize DataTypes are properly converted and thus:', function() {
-      if (supportedDataType('CITEXT')) {
-        describe('CITEXT', function() {
-          it("has property 'type' of type 'string'", function() {
-            expect(schema.properties.CITEXT.type).toEqual('string');
-          });
-        });
-      }
-
-      if (supportedDataType('INTEGER')) {
-        describe('INTEGER', function() {
-          it("has property 'default' with integer value 0", function() {
-            expect(schema.properties.INTEGER.default).toEqual(0);
-          });
-        });
-      }
-
-      if (supportedDataType('STRING')) {
-        describe('STRING', function() {
-          it("has property 'type' of type 'string'", function() {
-            expect(schema.properties.STRING.type).toEqual('string');
-          });
-
-          it("has property 'default' with string value 'Default value for STRING'", function() {
-            expect(schema.properties.STRING.default).toEqual('Default value for STRING');
-          });
+      describe('STRING_ALLOWNULL', function() {
+        it("has property 'type' of type 'array'", function() {
+          expect(Array.isArray(schema.properties.STRING_ALLOWNULL.type)).toBe(true);
         });
 
-        describe('STRING_1234', function() {
-          it("has property 'type' of type 'string'", function() {
-            expect(schema.properties.STRING_1234.type).toEqual('string');
-          });
-
-          it("has property 'maxLength' with value '1234'", function() {
-            expect(schema.properties.STRING_1234.maxLength).toEqual(1234);
-          });
-        });
-
-        describe('STRING_DOT_BINARY', function() {
-          it("has property 'type' of type 'string'", function() {
-            expect(schema.properties.STRING_DOT_BINARY.type).toEqual('string');
-          });
-
-          it("has property 'format' of type 'binary'", function() {
-            expect(schema.properties.STRING_DOT_BINARY.format).toEqual('binary');
-          });
-        });
-
-        describe('STRING_ALLOWNULL', function() {
-          it("has property 'type' of type 'array'", function() {
-            expect(Array.isArray(schema.properties.STRING_ALLOWNULL.type)).toBe(true);
-          });
-
-          it("has property 'type' with two values named 'string' and 'null'", function() {
-            expect(Object.values(schema.properties.STRING_ALLOWNULL.type)).toEqual([
-              'string',
-              'null',
-            ]);
-          });
-        });
-      }
-
-      if (supportedDataType('TEXT')) {
-        describe('TEXT', function() {
-          it("has property 'type' of type 'string'", function() {
-            expect(schema.properties.TEXT.type).toEqual('string');
-          });
-        });
-      }
-    });
-
-    // ------------------------------------------------------------------------
-    // make sure sequelize attribute options render as expected
-    // ------------------------------------------------------------------------
-    describe('Ensure Sequelize attributes options render as expected and thus:', function() {
-      if (supportedDataType('INTEGER')) {
-        describe('INTEGER with defaultValue', function() {
-          it("has property 'default' with integer value 0", function() {
-            expect(schema.properties.INTEGER.default).toEqual(0);
-          });
-        });
-      }
-
-      if (supportedDataType('STRING')) {
-        describe('STRING with defaultValue', function() {
-          it("has property 'default' with string value 'Default value for STRING'", function() {
-            expect(schema.properties.STRING.default).toEqual('Default value for STRING');
-          });
-        });
-      }
-    });
-
-    // ------------------------------------------------------------------------
-    // confirm user-definable attribute properties render as expected
-    // ------------------------------------------------------------------------
-    describe('Ensure user-enriched Sequelized attributes are properly converted and thus:', function() {
-      describe('USER_ENRICHED_PROPERTIES', function() {
-        it("has property 'description' of type 'string'", function() {
-          expect(typeof schema.properties.USER_ENRICHED_PROPERTIES.description).toBe('string');
-        });
-
-        it("has property 'examples' of type 'array'", function() {
-          expect(Array.isArray(schema.properties.USER_ENRICHED_PROPERTIES.examples)).toBe(true);
+        it("has property 'type' with two values named 'string' and 'null'", function() {
+          expect(Object.values(schema.properties.STRING_ALLOWNULL.type)).toEqual([
+            'string',
+            'null',
+          ]);
         });
       });
+    });
+
+    // ------------------------------------------------------------------------
+    // make sure user-definable attribute properties render as expected
+    // ------------------------------------------------------------------------
+    describe('Ensure user-enriched Sequelized attributes are properly converted and thus:', function() {
+      describe('USER_ENRICHED_ATTRIBUTE', function() {
+        it("has property 'examples' of type 'array'", function() {
+          expect(Array.isArray(schema.properties.USER_ENRICHED_ATTRIBUTE.examples)).toBe(true);
+        });
+      });
+    });
+
+    // ------------------------------------------------------------------------
+    // make sure associations render as expected
+    // ------------------------------------------------------------------------
+    describe('Ensure associations are properly generated and thus:', function() {
+      // @todo
     });
 
     // ------------------------------------------------------------------------
--- test/strategy-interface.test.js
+++ test/strategy-interface.test.js
@@ -1,12 +1,18 @@
+/**
+ * This test case is used to make sure:
+ * - child strategies will trigger an excecption if they miss an interface method
+ * - all non-strategy-specific model properties are rendered as expected
+ * - all non-strategy-specific attribute properties are rendered as expected
+ */
+const { SchemaManager, JsonSchema7Strategy } = require('../lib');
+const models = require('./models');
 const StrategyInterface = require('../lib/strategy-interface');
-
-// create a dummy strategy so we can test non-implemented methods
-class DummyStrategy extends StrategyInterface {}
-const dummyStrategy = new DummyStrategy();
+const supportedDataType = require('./utils/supported-datatype');
 
 describe('StrategyInterface', function() {
   // ------------------------------------------------------------------------
-  //  make sure we are testing what we expect
+  // make sure we are testing the expected number of interface methods. If
+  // this test fails, the exception tests likely need updating as well.
   // ------------------------------------------------------------------------
   describe('Ensure we are testing against:', function() {
     const methodCount = Object.getOwnPropertyNames(StrategyInterface.prototype).length - 1; // excluding the constructor
@@ -17,7 +23,13 @@ describe('StrategyInterface', function() {
   });
 
   // ------------------------------------------------------------------------
-  // test missing methods in the extended class
+  // create a dummy strategy without any implemented methods.
+  // ------------------------------------------------------------------------
+  class DummyStrategy extends StrategyInterface {}
+  const dummyStrategy = new DummyStrategy();
+
+  // ------------------------------------------------------------------------
+  // make sure exceptions are thrown when an interface method is missing.
   // ------------------------------------------------------------------------
   describe('Ensure exceptions are thrown if child class has not implemented:', function() {
     it('getPropertySchema()', function() {
@@ -60,6 +72,106 @@ describe('StrategyInterface', function() {
       }).toThrow(
         "DummyStrategy has not implemented the 'getPropertyForHasManyAssociation' interface method.",
       );
+    });
+  });
+
+  // ------------------------------------------------------------------------
+  // generate a JSON Draft-07 schema to test against
+  // ------------------------------------------------------------------------
+  const schemaManager = new SchemaManager();
+  const schema = schemaManager.generate(models.user, new JsonSchema7Strategy());
+
+  // ------------------------------------------------------------------------
+  // make sure non-strategy-specific model properties render as expected
+  // ------------------------------------------------------------------------
+  describe('Ensure model properties are properly generated and thus:', function() {
+    it("model has property 'title' with value 'users'", function() {
+      expect(schema.title).toEqual('User');
+    });
+
+    it("model has property 'type' with value 'object'", function() {
+      expect(schema.type).toEqual('object');
+    });
+  });
+
+  // ------------------------------------------------------------------------
+  // make sure non-strategy-specific attributes properties render as expected
+  // ------------------------------------------------------------------------
+  describe('Ensure non-strategy-specific Sequelize DataTypes are properly converted and thus:', function() {
+    if (supportedDataType('CITEXT')) {
+      describe('CITEXT', function() {
+        it("has property 'type' of type 'string'", function() {
+          expect(schema.properties.CITEXT.type).toEqual('string');
+        });
+      });
+    }
+
+    if (supportedDataType('STRING')) {
+      describe('STRING', function() {
+        it("has property 'type' of type 'string'", function() {
+          expect(schema.properties.STRING.type).toEqual('string');
+        });
+      });
+
+      describe('STRING_1234', function() {
+        it("has property 'type' of type 'string'", function() {
+          expect(schema.properties.STRING_1234.type).toEqual('string');
+        });
+
+        it("has property 'maxLength' with value '1234'", function() {
+          expect(schema.properties.STRING_1234.maxLength).toEqual(1234);
+        });
+      });
+
+      describe('STRING_DOT_BINARY', function() {
+        it("has property 'type' of type 'string'", function() {
+          expect(schema.properties.STRING_DOT_BINARY.type).toEqual('string');
+        });
+
+        it("has property 'format' of type 'binary'", function() {
+          expect(schema.properties.STRING_DOT_BINARY.format).toEqual('binary');
+        });
+      });
+    }
+
+    if (supportedDataType('TEXT')) {
+      describe('TEXT', function() {
+        it("has property 'type' of type 'string'", function() {
+          expect(schema.properties.TEXT.type).toEqual('string');
+        });
+      });
+    }
+  });
+
+  // ------------------------------------------------------------------------
+  // make sure sequelize attribute options render as expected
+  // ------------------------------------------------------------------------
+  describe('Ensure Sequelize attributes options render as expected and thus:', function() {
+    if (supportedDataType('INTEGER')) {
+      describe('INTEGER with defaultValue', function() {
+        it("has property 'default' with integer value 0", function() {
+          expect(schema.properties.INTEGER.default).toEqual(0);
+        });
+      });
+    }
+
+    if (supportedDataType('STRING')) {
+      describe('STRING with defaultValue', function() {
+        it("has property 'default' with string value 'Default value for STRING'", function() {
+          expect(schema.properties.STRING.default).toEqual('Default value for STRING');
+        });
+      });
+    }
+  });
+
+  // ------------------------------------------------------------------------
+  // confirm user-definable attribute properties render as expected
+  // ------------------------------------------------------------------------
+  describe('Ensure user-enriched Sequelized attributes are properly converted and thus:', function() {
+    describe('USER_ENRICHED_ATTRIBUTE', function() {
+      it("has property 'description' of type 'string'", function() {
+        expect(typeof schema.properties.USER_ENRICHED_ATTRIBUTE.description).toBe('string');
+      });
     });
   });
 });
